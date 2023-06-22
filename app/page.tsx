7bc3426efa8e75be46906a738a7d22466b52180c
TheMisterMatthewMan <114592934+TheMisterMatthewMan@users.noreply.github.com>
2023-06-22 13:35:04 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,44 @@
 import Image from 'next/image'
 
 export default function Home() {
+
+  const container = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: '900px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: '1rem'
+  }
+
+  const wrapper = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+  const subContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: '1rem'
+  }
+
+  const heading = {
+    fontSize: '3rem',
+    fontWeight: '600'
+  }
+
   return (
-    <div className="flex flex-col items-center p-7">
-      <div className="flex flex-row">
+    <div style= {container}>
+      <div style= {wrapper}>
         <div className="image">
           <img height='200px' width='200px' src="/profile.webp"></img>
         </div>
-        <div className="flex flex-col justify-center p-7">
-          <h1 className="text-7xl font-extrabold">Hi, my name is Matthew</h1>
+        <div style= {subContainer}>
+          <h1 style= {heading}>Hi, my name is Matthew</h1>
           <p>I am a student at EIS.</p>
           <p>I am taking web design classes at Kiddee Lab.</p>
         </div>
